724a6eb81ab4d97afbbef563fb52d9e56191c2a5
Scott Schwartz <schwartz.m.scott@gmail.com>
2020-01-12 14:18:08 -0800

    update

--- src/modules/Home/Latest/Container.tsx
+++ src/modules/Home/Latest/Container.tsx
@@ -6,7 +6,7 @@ interface LatestContainerProps {}
 
 export class LatestContainer extends React.PureComponent<LatestContainerProps> {
   state = {
-    expanded: true
+    expanded: false
   };
 
   toggleExpanded = () => {
@@ -25,6 +25,30 @@ export class LatestContainer extends React.PureComponent<LatestContainerProps> {
           </div>
         </div>
         <div className="home-latest-list">
+          <GameCard
+            expanded={this.state.expanded}
+            name="Halo Infinite"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQxHBC_cbWak2wpF82npTqpI1wCzvkIVAVVwTJJLNWv3hztYDeK"
+            releaseDate="Jan 21"
+          />
+          <GameCard
+            expanded={this.state.expanded}
+            name="Forza Horizon 5"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRmXICnOt00miUIXNMPiDuLyLjuOwcxtdHM8lHDupvODvuJgevq"
+            releaseDate="Jan 21"
+          />
+          <GameCard
+            expanded={this.state.expanded}
+            name="Senua's Saga: Hellblade 2"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSRV2_AGedNvEcNpFRuaGwY1aeus-cCoLPzkzVUgpqRlEJhezX1"
+            releaseDate="Jan 21"
+          />
+          <GameCard
+            expanded={this.state.expanded}
+            name="FIFA 2021"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTy09ZZ7YsucvGoU_0cT-G5ZMiQkbiXpGgUKFfL0483h2pefRV6"
+            releaseDate="Jan 21"
+          />
           <GameCard
             expanded={this.state.expanded}
             name="Halo Infinite"
